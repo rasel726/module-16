41d71536418bfc28b7a8c43f6d116e563c54c2c0
rasel726 <md.rasel2726@gmail.com>
2022-08-02 12:25:53 +0600

    added

--- variable.js
+++ variable.js
@@ -1,7 +1,19 @@
 // vary + able = variable
+
+// numeric
 var price = 11;
 var age = 17;
 var temparature = 38;
 var roll = 10;
 var reg = 625455;
 var id = 212010122;
+
+// string
+var person = "kamrul islam";
+var location = "vatara dhaka";
+var special = 'father';
+
+var serious = true;
+var isFullmarks = true;
+var is isSingle = false;
+
