7ce6e27a98d0b8d5e6789b450485d98196f6ff9b
rasel726 <md.rasel2726@gmail.com>
2022-08-02 18:57:27 +0600

    added

---
+++ variable-name.js
@@ -0,0 +1,22 @@
+// good variable
+var price = 29;
+
+// 1. variable name can not be any keywords
+var false = 96;
+var return = true;
+
+// 2. variable name has to be in one work. No space
+var my home address = "New California";
+
+// 3. variable name can not habe quotation
+var "name" = "Tom Haks";
+
+// 4. variable name can not starts with a number but can ends  with a number
+var 99club = 1964;
+var club25 = 2025;
+
+// 5. how to use long names
+// can not use dash
+var user-name = "raj bappa";
+
+var user_name = "bappa raj";
