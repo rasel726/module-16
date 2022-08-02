a512cd6be74b46b0bc06eeccf2256a40ecddff10
rasel726 <md.rasel2726@gmail.com>
2022-08-02 10:02:30 +0600

    math js file added

---
+++ math.js
@@ -0,0 +1,18 @@
+/***
+ * 1. number ==> 55. 898, -32
+ * 2. integer, odd, even : 2, 5,
+ * 3. float decimal: 2.5, 13.67, 32.69
+ * 4. operation: addition, substraction, multiplication, division, remainder
+ * 5. percent, 
+ * 6. average
+ * 7. comparison: >, <, !=, >=, <=
+ * 8. oikik: 2 kilo apple and 5 kilo mango: 
+ * 9. 45, 45, 68, 68, 129, 2: max, min, avarage, sum
+ * 10. series:
+ * :::EXTRA:::
+ * 1. Prime
+ * 2. premutation
+ * 3. Area: 
+ * :::ALWAYS:::
+ * Jodi lage tahole seta obossoi jante hobe...
+*/
